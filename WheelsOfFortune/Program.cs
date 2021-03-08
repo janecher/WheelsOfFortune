1aec8b4f61855ecc6492ae1fb97b8087176edd57
Evgeniya Chernaya <v-echernaya@microsoft.com>
2021-03-07 17:28:33 -0800

    initial commit

---
+++ WheelsOfFortune/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WheelsOfFortune
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
