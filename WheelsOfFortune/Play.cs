009c41ce56ca2d89c09c588480d724a2db7adff4
Evgeniya Chernaya <v-echernaya@microsoft.com>
2021-03-07 18:26:39 -0800

    add play class that implement all main gaim functionality

---
+++ WheelsOfFortune/Play.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WheelsOfFortune
+{
+    public class Play
+    {
+        private string[] words = new string[] { "apple", "banana", "orange", "cucumber", "strawberry", "blackberry" };
+
+        public string RandomWord()
+        {
+            Random rand = new Random();
+            int index = rand.Next(words.Length);
+            return words[index];
+        }
+
+        public string MakeStringDashes(string input)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                result.Append("-");
+            }
+            return result.ToString();
+        }
+
+        public bool GuessResult(char guess, string origin)
+        {
+            return origin.Contains(guess);
+        }
+
+        public string GuessResult(char guess, string origin, string dashes)
+        {
+            char[] dashesChars = dashes.ToCharArray();
+            for (int i = 0; i < origin.Length; i++)
+            {
+                if (origin[i] == guess)
+                {
+                    dashesChars[i] = guess;
+                }
+            }
+            return dashesChars.ToString();
+        }
+
+        public bool StopGame(string dashes)
+        {
+            return dashes.Contains("-");
+        }
+    }
+}
