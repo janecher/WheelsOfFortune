5fa6e59fcd2ba116531dbd8f0359cc908c173cf5
Evgeniya Chernaya <v-echernaya@microsoft.com>
2021-03-07 18:04:29 -0800

    create player class with name property and guessletter method

---
+++ WheelsOfFortune/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WheelsOfFortune
+{
+    public class Player
+    {
+        public string Name { get; set; }
+
+        public char GetGuessLetter()
+        {
+            Console.WriteLine("Guess letter?");
+            string letter = Console.ReadLine();
+            if (!Validation(letter))
+            {
+                return GetGuessLetter();
+            }
+            else
+            {
+                return letter.ToLower()[0];
+            }
+        }
+
+        private bool Validation(string input)
+        {
+            if (input.Length != 1)
+            {
+                Console.WriteLine("Incorrect input. Only 1 letter allow.");
+                return false;
+            }
+            if (!char.IsLetter(input[0]))
+            {
+                Console.WriteLine("Incorrect input. Only letters allow.");
+                return false;
+            }
+            return true;
+        }
+    }
+}
